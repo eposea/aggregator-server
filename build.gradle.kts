8ba0122147f208394d2ece3be17728c9b67faaa7
blackkiv <fallena238@gmail.com>
2022-02-06 23:03:12 +0200

    🎉 init

---
+++ build.gradle.kts
@@ -0,0 +1,76 @@
+plugins {
+    id("org.jetbrains.kotlin.jvm") version "1.6.10"
+    id("org.jetbrains.kotlin.kapt") version "1.6.10"
+    id("org.jetbrains.kotlin.plugin.allopen") version "1.6.10"
+    id("com.github.johnrengelman.shadow") version "7.1.1"
+    id("io.micronaut.application") version "3.2.0"
+}
+
+version = "0.1"
+group = "eposea"
+
+val kotlinVersion = project.properties.get("kotlinVersion")
+val coherenceVersion = project.properties.get("coherenceVersion")
+
+repositories {
+    mavenCentral()
+}
+dependencies {
+    kapt("io.micronaut:micronaut-http-validation")
+    kapt("io.micronaut.data:micronaut-data-processor")
+    implementation("com.oracle.coherence.ce:coherence:${coherenceVersion}")
+    implementation("com.oracle.coherence.ce:coherence-java-client:${coherenceVersion}")
+    implementation("io.micronaut:micronaut-http-client")
+    implementation("io.micronaut:micronaut-jackson-databind")
+    implementation("io.micronaut:micronaut-runtime")
+    implementation("io.micronaut.coherence:micronaut-coherence")
+    implementation("io.micronaut.coherence:micronaut-coherence-grpc-client")
+    implementation("io.micronaut.data:micronaut-data-jdbc")
+    implementation("io.micronaut.kotlin:micronaut-kotlin-extension-functions")
+    implementation("io.micronaut.kotlin:micronaut-kotlin-runtime")
+    implementation("io.micronaut.sql:micronaut-jdbc-hikari")
+    implementation("jakarta.annotation:jakarta.annotation-api")
+    implementation("org.jetbrains.kotlin:kotlin-reflect:${kotlinVersion}")
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8:${kotlinVersion}")
+    runtimeOnly("org.postgresql:postgresql")
+    runtimeOnly("org.slf4j:slf4j-simple")
+    testImplementation("org.testcontainers:junit-jupiter")
+    testImplementation("org.testcontainers:postgresql")
+    testImplementation("org.testcontainers:testcontainers")
+    implementation("io.micronaut:micronaut-validation")
+
+    runtimeOnly("com.fasterxml.jackson.module:jackson-module-kotlin")
+
+}
+
+
+application {
+    mainClass.set("eposea.ApplicationKt")
+}
+java {
+    sourceCompatibility = JavaVersion.toVersion("11")
+}
+
+tasks {
+    compileKotlin {
+        kotlinOptions {
+            jvmTarget = "11"
+        }
+    }
+    compileTestKotlin {
+        kotlinOptions {
+            jvmTarget = "11"
+        }
+    }
+}
+graalvmNative.toolchainDetection.set(false)
+micronaut {
+    runtime("jetty")
+    testRuntime("junit5")
+    processing {
+        incremental(true)
+        annotations("eposea.*")
+    }
+}
+
+
---
+++ src/main/kotlin/eposea/Application.kt
@@ -0,0 +1,11 @@
+package eposea
+
+import io.micronaut.runtime.Micronaut.*
+
+fun main(args: Array<String>) {
+    build()
+        .args(*args)
+        .packages("eposea")
+        .start()
+}
+
