f091ed891c5fb4532dcddd88cb4009188a2d6841
blackkiv <fallena238@gmail.com>
2022-02-15 23:13:19 +0200

    add grpc support and common submodule

--- aggregator/build.gradle.kts
+++ aggregator/build.gradle.kts
@@ -1,16 +1,24 @@
+import com.google.protobuf.gradle.*
+import org.gradle.kotlin.dsl.invoke
+
 plugins {
     id("org.jetbrains.kotlin.jvm") version "1.6.10"
     id("org.jetbrains.kotlin.kapt") version "1.6.10"
     id("org.jetbrains.kotlin.plugin.allopen") version "1.6.10"
     id("com.github.johnrengelman.shadow") version "7.1.1"
     id("io.micronaut.application") version "3.2.0"
+    id("com.google.protobuf") version "0.8.18"
 }
 
 version = "0.1"
 group = "eposea"
 
 val kotlinVersion = project.properties["kotlinVersion"]
-val coherenceVersion = project.properties["coherenceVersion"]
+val coherenceVersion = "21.06.2"
+val grpcVersion = "1.44.0"
+val grpcKotlinVersion = "1.2.1"
+val protobufVersion = "3.19.2"
+val protocVersion = protobufVersion
 
 repositories {
     mavenCentral()
@@ -32,6 +40,9 @@ dependencies {
     implementation("jakarta.annotation:jakarta.annotation-api")
     implementation("org.jetbrains.kotlin:kotlin-reflect:${kotlinVersion}")
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8:${kotlinVersion}")
+    implementation("io.grpc:grpc-protobuf:${grpcVersion}")
+    implementation("io.grpc:grpc-stub:${grpcVersion}")
+    implementation("io.grpc:grpc-kotlin-stub:${grpcKotlinVersion}")
     runtimeOnly("org.postgresql:postgresql")
     runtimeOnly("org.slf4j:slf4j-simple")
     testImplementation("org.testcontainers:junit-jupiter")
@@ -73,4 +84,38 @@ micronaut {
     }
 }
 
+protobuf {
+    protoc {
+        artifact = "com.google.protobuf:protoc:${protocVersion}"
+    }
+    plugins {
+        id("grpc") {
+            artifact = "io.grpc:protoc-gen-grpc-java:${grpcVersion}"
+        }
+        id("grpckt") {
+            artifact = "io.grpc:protoc-gen-grpc-kotlin:${grpcKotlinVersion}:jdk7@jar"
+        }
+    }
+    generateProtoTasks {
+        all().forEach {
+            it.plugins {
+                id("grpc")
+                id("grpckt")
+            }
+            it.builtins {
+                id("kotlin")
+            }
+        }
+    }
+}
 
+sourceSets {
+    main {
+        java {
+            srcDirs("build/generated/source/proto/main/grpc")
+            srcDirs("build/generated/source/proto/main/grpckt")
+            srcDirs("build/generated/source/proto/main/java")
+            srcDirs("src/common/proto")
+        }
+    }
+}
